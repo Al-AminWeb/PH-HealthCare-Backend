abaec5c7a918dfd1dd7c8aa2efdd47db0a0a3678
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-02 23:10:53 +0600

    Added ESLint configuration file with TypeScript support and strict linting rules

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import type {Application} from "express";
+import express from "express";
+
+const app:Application = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+
+export default app;
---
+++ src/app/server.ts
@@ -0,0 +1,16 @@
+import type {Server} from "http";
+import app from "./app.js";
+
+
+async function bootstrap() {
+    const server: Server | null = null;
+    try {
+        server = app.listen(5000, () => {
+            console.log("Server is running on port 5000");
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+bootstrap();
