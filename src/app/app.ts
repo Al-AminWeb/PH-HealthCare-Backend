7f4a1846ddcc4aa641ca834f8a14149bf09e2279
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-03 22:51:19 +0600

    Integrated Prisma and BetterAuth for database and authentication setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,13 @@
 import type {Application} from "express";
 import express from "express";
+import {prisma} from "../shared/prisma.js";
+import {toNodeHandler} from "better-auth/node";
+import {auth} from "../lib/auth.js";
+const app: Application = express();
 
-const app:Application = express();
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
--- src/app/server.ts
+++ src/app/server.ts
@@ -3,7 +3,7 @@ import app from "./app.js";
 
 
 async function bootstrap() {
-    const server: Server | null = null;
+    let server: Server | null = null;
     try {
         server = app.listen(5000, () => {
             console.log("Server is running on port 5000");
